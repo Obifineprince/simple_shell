fad526e86cae843e7f300ca4513277a4a4633c7f
SHEDDY04 <chukwuebukasheddy@gmail.com>
2023-05-24 10:23:21 -0700

    simple shell

--- 0x02-builtin.c
+++ 0x02-builtin.c
@@ -1,14 +1,14 @@
 #include "shell.h"
+
 /**
  * builtin_env - displays the environment in which the shell is running
  * @input: struct containing program's data
- * Return: zero if successful, or non-zero if specified in the arguments
+ * Return: 0 if successful, or non-zero if specified in the arguments
  */
-
 int builtin_env(shell_program *input)
 {
-	int j;
-	char spname[50] = {'\0'};
+	int i;
+	char cpname[50] = {'\0'};
 	char *var_copy = NULL;
 
 	/* if not arguments */
@@ -16,30 +16,30 @@ int builtin_env(shell_program *input)
 		print_environ(input);
 	else
 	{
-		for (j = 0; input->tokens[1][j]; j++)
-		{/* Checks if a character '=' exists */
-			if (input->tokens[1][j] == '=')
-		{
-/* Checks if a variable with same name exists &temporarily changes its value */
-				var_copy = str_duplic(env_get_key(spname, input));
+		for (i = 0; input->tokens[1][i]; i++)
+		{/* checks if exists a char = */
+			if (input->tokens[1][i] == '=')
+			{/* checks if exists a var with the same name and change its value*/
+			/* temporally */
+				var_copy = str_duplicate(env_get_key(cpname, input));
 				if (var_copy != NULL)
-					env_set_key(spname, input->tokens[1] + j + 1, input);
+					env_set_key(cpname, input->tokens[1] + i + 1, input);
 
-				/* display the environ */
+				/* print the environ */
 				print_environ(input);
-				if (env_get_key(spname, input) == NULL)
-				{/* Prints the variable if it doesn’t exist in the environment */
+				if (env_get_key(cpname, input) == NULL)
+				{/* print the variable if it does not exist in the environ */
 					_print(input->tokens[1]);
 					_print("\n");
 				}
 				else
-				{/* Returns the previous value of the variable */
-					env_set_key(spname, var_copy, input);
+				{/* returns the old value of the var*/
+					env_set_key(cpname, var_copy, input);
 					free(var_copy);
 				}
 				return (0);
 			}
-			spname[j] = input->tokens[1][j];
+			cpname[i] = input->tokens[1][i];
 		}
 		errno = 2;
 		perror(input->command_name);
@@ -47,15 +47,15 @@ int builtin_env(shell_program *input)
 	}
 	return (0);
 }
+
 /**
  * builtin_set_env - Function to perform a specific task in the shell
  * @input: Pointer to the struct holding program data.
  * Return: Integer indicating success (zero) or failure (non-zero).
  */
-
 int builtin_set_env(shell_program *input)
 {
-	/* Validate argument */
+	/* validate args */
 	if (input->tokens[1] == NULL || input->tokens[2] == NULL)
 		return (0);
 	if (input->tokens[3] != NULL)
@@ -75,10 +75,9 @@ int builtin_set_env(shell_program *input)
  * @input: Pointer to the struct holding program data.
  * Return: Integer indicating success (zero) or failure (non-zero).
  */
-
 int builtin_unset_env(shell_program *input)
 {
-	/* Validate Arguments */
+	/* validate args */
 	if (input->tokens[1] == NULL)
 		return (0);
 	if (input->tokens[2] != NULL)
